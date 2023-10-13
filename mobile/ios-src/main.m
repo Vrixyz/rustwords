94f2f13e41d37fdf0d92a746172c8abf5b1ce97a
Thierry Berger <contact@thierryberger.com>
2023-10-13 12:58:48 +0200

    fix ad view size

--- mobile/ios-src/main.m
+++ mobile/ios-src/main.m
@@ -3,6 +3,8 @@
 #import "bindings.h"
 
 int main() {
+    NSLog(@"AppLovinSdkKey:");
+    //NSLog([[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppLovinSdkKey"]);
     main_rs();
     return 0;
 }
@@ -14,9 +16,11 @@ void display_ad(UIWindow* window, UIViewController* viewController) {
         NSLog( @"Ad already showing.");
         return;
     }
-    NSLog( @"Showing an ad." );
+    NSLog( @"viewController dimensions:%i;%i",  viewController.view.frame.size.width,viewController.view.frame.size.height );
     shownAd = [[[NSBundle mainBundle] loadNibNamed:@"Ad" owner:viewController options:nil] objectAtIndex:0];
-    [window.rootViewController.view addSubview:shownAd];
+    NSLog( @"shownAd dimensions:%i;%i",  viewController.view.frame.size.width,shownAd.frame.size.height );
+    shownAd.frame = viewController.view.frame;
+    [viewController.view addSubview:shownAd];
 }
 
 void close_ad() {
